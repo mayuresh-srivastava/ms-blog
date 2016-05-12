54350a8b4f07f5c9779b32e58f52b6739a13d7a7
Mayuresh Srivastava <mayureshmayurs@gmail.com>
2016-05-12 07:27:23 +0000

    modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  def full_name
-    return first_name + " " + last_name
-  end  
+#  def full_name
+#    return first_name + ' ' + last_name
+#  end  
 end
